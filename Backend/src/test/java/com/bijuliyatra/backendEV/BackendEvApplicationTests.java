84c253730990fd44c0ef9f33fe904961c640498a
Thepe314 <74044887+Thepe314@users.noreply.github.com>
2025-10-01 22:30:30 +0545

    set up of files
    frontend and backend setup

---
+++ Backend/src/test/java/com/bijuliyatra/backendEV/BackendEvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bijuliyatra.backendEV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
